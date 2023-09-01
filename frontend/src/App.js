b9a46e7cb060d97ed48968989e6c4afaabe033fd
Irfan Shaikh <irfansk.se@gmail.com>
2023-09-01 06:42:56 +0400

    starting the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
-export function App() {
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
   return (
-    <div>Pro Shop</div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
+
+export default App
